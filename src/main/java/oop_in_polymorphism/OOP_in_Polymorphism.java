14327ea249e01907048dc43310431732a3a41e7e
offic <offic@DESKTOP-GLF4UNU>
2024-07-29 18:40:36 +0600

    Types of polymorphism.

--- src/main/java/oop_in_polymorphism/OOP_in_Polymorphism.java
+++
@@ -1,7 +0,0 @@
-package oop_in_polymorphism;
-public class OOP_in_Polymorphism {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome! Introduction to \"object oriented programming_OOP\".");
-    }
-}
---
+++ src/main/java/oop_in_polymorphism/Types_of_polymorphism.java
@@ -0,0 +1,32 @@
+package oop_in_polymorphism;
+public class Types_of_polymorphism {
+
+    /*
+    object oriented programming_OOP:
+    object oriented programming প্রধানত ৪ প্রকার, 
+        Encapsulation
+        Inheritance
+        Polymorphism
+        Abstraction
+    
+    java-তে কাজ করার জন্য এই Access modifier গুলোর প্রয়োজন পড়ে,
+    There are four types of access modifier in java:
+        private
+        default
+        protected
+        public
+    Access modifier গুলো কোন variable / method এর আগে বসানো হয়।।
+    
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++
+    Polymorphism দু’ধরনের: 
+    Compile Time polymorphism / static polymorphism
+    Run Time polymorphism / dynamic polymorphism
+    
+    override method কে run time polymorphism বলে।।
+    overloading method, overloading constructor কে static polymorphism বলে।।
+    
+    super class এর variable দ্বারা সকল ‍sub-class এর object কে declare করা যায়।। এটাই হলো polymorphism.
+    
+    super class এর variable দ্বারা সকল ‍sub-class এর object কে declare করা যায়।। এটা হলো polymorphism. আর এটাকে runtime polymorphism বলে।।
+     */
+}
