b5eda7595f572d264276994c9030f9757e1c5059
offic <offic@DESKTOP-GLF4UNU>
2024-07-29 18:22:15 +0600

    Welcome! Introduction to "OOP in polymorphism"

---
+++ src/main/java/oop_in_polymorphism/OOP_in_Polymorphism.java
@@ -0,0 +1,7 @@
+package oop_in_polymorphism;
+public class OOP_in_Polymorphism {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"object oriented programming_OOP\".");
+    }
+}
